326e8fad862f69ae9c48f93e9cf5d27ae83c4f6a
holmen1 <holmen1@gmail.com>
2024-08-29 12:42:39 +0200

    fix

--- 6_Higher_order_functions/curry.hs
+++ 6_Higher_order_functions/curry.hs
@@ -1,5 +1,16 @@
+-- By calling functions with too few parameters, so to speak, we're creating new functions on the fly.
+-- What if we wanted to create a function that takes a number and compares it to 100?
+-- We could do something like this:
 compareWithHundred :: (Num a, Ord a) => a -> Ordering  
 compareWithHundred x = compare 100 x
+
+-- If we call it with 99, it returns a GT. Simple stuff. Notice that the x is on the right
+-- hand side on both sides of the equation. Now let's think about what compare 100 returns.
+-- It returns a function that takes a number and compares it with 100.
+-- Wow! Isn't that the function we wanted? We can rewrite this as:
+compareWithHundred' :: (Num a, Ord a) => a -> Ordering
+compareWithHundred' = compare 100
+-- The type declaration stays the same, because compare 100 returns a function. Compare has a type of (Ord a) => a -> (a -> Ordering) and calling it with 100 returns a (Num a, Ord a) => a -> Ordering. The additional class constraint sneaks up there because 100 is also part of the Num typeclass.
 
 isUpperAlphanum :: Char -> Bool  
 isUpperAlphanum = (`elem` ['A'..'Z'])
--- 9_Input_and_Output/forM.hs
+++ 9_Input_and_Output/forM.hs
@@ -7,10 +7,9 @@ switched around. The first parameter is the list and the second one is the
 function to map over that list, which is then sequenced. -}
   
 main = do   
-    colors <- forM [1,2,3,4] (\a -> do  
+    colors <- forM [1,2,3,4] (\a -> do
         putStrLn $ "Which color do you associate with the number " ++ show a ++ "?"  
-        color <- getLine  
-        return color)  
+        getLine)  
     putStrLn "The colors that you associate with 1, 2, 3 and 4 are: "  
     mapM putStrLn colors
 
